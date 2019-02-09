28384c34a57c937e880f643065c9c6bfb81c6297
RussFerns <russferns@gmail.com>
2019-02-08 19:37:49 -0500

    fixed more checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  // removed old comment and added this line
+  /** {@inheritDoc} */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,4 @@
-/**
+/** created 2/8/2019 by RF.
  * This is a sample package-info file
  */
  package com.example;
- 
